01faee8bfc4cb9b8d92a6f43425e4411f016e999
Egor Ermilov <egorka33rus52@gmail.com>
2021-02-01 23:11:33 +0300

    Framework (#3)
    * created components and core
    
    * completed the formation of the functional framework

---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,37 @@
+import { ExcelComponent } from '../../core/ExcelComponent';
+
+export class Toolbar extends ExcelComponent {
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click'],
+    });
+  }
+  static className = 'excel__toolbar';
+  toHTML() {
+    return `          <div class="excel__toolbar-btns">
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_align_left </span>
+    </div>
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_align_center </span>
+    </div>
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_align_right </span>
+    </div>
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_bold </span>
+    </div>
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_italic </span>
+    </div>
+    <div class="excel__toolbar-btn">
+      <span class="material-icons"> format_underlined </span>
+    </div>
+  </div>`;
+  }
+
+  onClick(event) {
+    console.log(event.target.textContent.trim());
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+'use strict';
+
+import { Excel } from './components/excel/Excel';
+
+import { Header } from './components/header/Header';
+import { Toolbar } from './components/toolbar/Toolbar';
+import { Formula } from './components/formula/Formula';
+import { Table } from './components/table/Table';
+
 import './scss/index.scss';
 
-('use strict');
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
 
-console.log(`Всем привет`);
-console.log(`Всем привет`);
+excel.render();
