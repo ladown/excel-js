e7fca0a337768a7cbff04c006b3b3d452da19d3e
Егор Ермилов <egorka33rus52@gmail.com>
2021-02-07 13:48:35 +0300

    changed favicon and dashboard-title

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -8,7 +8,7 @@ export class DashboardPage extends Page {
     const now = Date.now().toString();
     return $.create('div', 'db').html(`        
     <div class="db__header">
-      <h1>Excel Dashboard</h1>
+      <h1>Панель управления JS-Excel</h1>
     </div>
     <div class="db__new">
       <div class="db__view">
